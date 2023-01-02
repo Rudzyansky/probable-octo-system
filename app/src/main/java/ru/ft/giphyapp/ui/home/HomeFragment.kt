ec16f7a0a8a3d5135e6bf47eba96a69d8e7ef26c
Evgeny <evgeny@false.team>
2023-01-02 18:23:43 +0300

    inject viewmodel factory

--- app/src/main/java/ru/ft/giphyapp/ui/home/HomeFragment.kt
+++ app/src/main/java/ru/ft/giphyapp/ui/home/HomeFragment.kt
@@ -10,11 +10,15 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.ft.giphyapp.App
 import ru.ft.giphyapp.R
 import ru.ft.giphyapp.databinding.FragmentHomeBinding
+import javax.inject.Inject
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private val viewModel: HomeViewModel by viewModels { HomeViewModel.Factory() }
+    private val viewModel: HomeViewModel by viewModels { vmFactory }
     private val binding by viewBinding(FragmentHomeBinding::bind)
+
+    @Inject
+    lateinit var vmFactory: HomeViewModel.Factory
 
     override fun onAttach(context: Context) {
         (requireActivity().application as App).appComponent.inject(this)
