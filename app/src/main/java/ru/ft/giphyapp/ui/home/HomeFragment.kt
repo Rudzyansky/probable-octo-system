7e0fbda37ec8c4cdec25e5a900006af350bc5677
Evgeny <evgeny@false.team>
2023-01-02 17:59:50 +0300

    Fix viewmodel creating (lifecycle)

--- app/src/main/java/ru/ft/giphyapp/ui/home/HomeFragment.kt
+++ app/src/main/java/ru/ft/giphyapp/ui/home/HomeFragment.kt
@@ -1,22 +1,34 @@
 package ru.ft.giphyapp.ui.home
 
 import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.ft.giphyapp.App
 import ru.ft.giphyapp.R
 import ru.ft.giphyapp.databinding.FragmentHomeBinding
-import javax.inject.Inject
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    @Inject
-    lateinit var viewModel: HomeViewModel
-
+    private val viewModel: HomeViewModel by viewModels { HomeViewModel.Factory() }
     private val binding by viewBinding(FragmentHomeBinding::bind)
 
     override fun onAttach(context: Context) {
         (requireActivity().application as App).appComponent.inject(this)
         super.onAttach(context)
+        Log.d("AAA", "Fragment onAttach")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("AAA", "Fragment onDetach")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.a
     }
 }
--- app/src/main/java/ru/ft/giphyapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/ru/ft/giphyapp/ui/home/HomeViewModel.kt
@@ -1,8 +1,29 @@
 package ru.ft.giphyapp.ui.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 
-class HomeViewModel @Inject constructor(
+class HomeViewModel(
 ) : ViewModel() {
+
+    val a = ""
+
+    init {
+        Log.d("AAA", "ViewModel init")
+    }
+
+    override fun onCleared() {
+        Log.d("AAA", "ViewModel onCleared")
+        super.onCleared()
+    }
+
+    class Factory @Inject constructor(
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            @Suppress("UNCHECKED_CAST")
+            return HomeViewModel() as T
+        }
+    }
 }
